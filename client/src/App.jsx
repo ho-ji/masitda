6f951aa3a504cd363c76b885513531500ffd7696
ho-ji <wkdd0205@gmail.com>
2024-05-06 18:12:17 +0900

    feat: 페이지 라우트 추가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from 'pages/Home'
 import GlobalStyle from 'styles/GlobalStyle'
 import 'styles/globalFont.css'
+import Cart from 'pages/Cart'
+import User from 'pages/User'
+import NotFound from 'pages/NotFound'
 
 const App = () => {
   return (
@@ -12,6 +15,18 @@ const App = () => {
         <Route
           path="/"
           element={<Home />}
+        />
+        <Route
+          path="/cart"
+          element={<Cart />}
+        />
+        <Route
+          path="/user"
+          element={<User />}
+        />
+        <Route
+          path="/*"
+          element={<NotFound />}
         />
       </Routes>
     </BrowserRouter>
---
+++ client/src/pages/User.jsx
@@ -0,0 +1,7 @@
+import Layout from 'components/common/Layout'
+
+const User = () => {
+  return <Layout></Layout>
+}
+
+export default User
