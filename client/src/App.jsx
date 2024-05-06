bb065d7f54e26e7ad0b2fb5c82a5c4c3ba50f1bb
ho-ji <wkdd0205@gmail.com>
2024-05-06 18:06:08 +0900

    style: 폰트 적용

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import Home from './pages/Home'
 import GlobalStyle from './styles/GlobalStyle'
+import './styles/globalFont.css'
 
 const App = () => {
   return (
