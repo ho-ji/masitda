9c79f60ebf58da653b6db31f88a74c6f1225ba20
ho-ji <wkdd0205@gmail.com>
2024-05-13 03:01:17 +0900

    refactor: bestList API와 topList를 하나로 합쳐서 limit로 구분되도록 수정

--- server/routes/mdPickRoutes.js
+++ server/routes/mdPickRoutes.js
@@ -3,10 +3,8 @@ const MDPick = require('../models/MDPick')
 
 router.get('/', async (req, res) => {
   try {
-    const limit = parseInt(req.query.limit)
-    const list = await MDPick.find({})
-      .populate('product')
-      .limit(limit || 0)
+    const limit = parseInt(req.query.limit) || 0
+    const list = await MDPick.find({}).populate('product').limit(limit)
     const products = list.map((item) => item.product)
     res.status(200).send(products)
   } catch (error) {
--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -3,18 +3,8 @@ const Product = require('../models/Product')
 
 router.get('/ranking', async (req, res) => {
   try {
-    const ranking = await Product.find({}).sort({sales_count: -1}).limit(50)
-    res.status(200).send(ranking)
-  } catch (error) {
-    res.status(500).json({
-      message: 'Ranking Not Found',
-    })
-  }
-})
-
-router.get('/top', async (req, res) => {
-  try {
-    const ranking = await Product.find({}).sort({sales_count: -1}).limit(6)
+    const limit = parseInt(req.query.limit) || 50
+    const ranking = await Product.find({}).sort({sales_count: -1}).limit(limit)
     res.status(200).send(ranking)
   } catch (error) {
     res.status(500).json({
