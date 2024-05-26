876ddb9fca0966113e7d543426bb6a3640b658b1
ho-ji <wkdd0205@gmail.com>
2024-05-26 20:40:43 +0900

    feat: 회원가입 페이지 계정소유 여부 후 로그인 페이지 랜더링 추가

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import AuthLayout from 'components/common/AuthLayout'
 import SignUpForm from 'components/signup/SignUpForm'
+import {Link} from 'react-router-dom'
 import styled from 'styled-components'
 
 const Title = styled.h2`
@@ -8,12 +9,29 @@ const Title = styled.h2`
   margin-bottom: 3rem;
   align-self: flex-start;
 `
+const CheckAccount = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  gap: 1rem;
+  > p {
+    color: var(--color-text-sub);
+    text-decoration: underline;
+  }
+  > a {
+    color: var(--color-main);
+    font-weight: bold;
+  }
+`
 
 const SignUp = () => {
   return (
     <AuthLayout>
       <Title>회원가입</Title>
       <SignUpForm />
+      <CheckAccount>
+        <p>이미 계정이 있나요?</p>
+        <Link to="/login">로그인</Link>
+      </CheckAccount>
     </AuthLayout>
   )
 }
