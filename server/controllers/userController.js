265fbdcb4cababf6226bdb75db209abf89b5cd9f
ho-ji <wkdd0205@gmail.com>
2024-06-04 01:46:51 +0900

    refactor: 4xx에러코드 시 콘솔창 기본에러메시지로 인한 200처리 후 success

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,12 +6,13 @@ const postUserSignUp = async (req, res) => {
   try {
     const user = await service.getUserByAccount(info.account)
     if (user) {
-      return res.status(409).json({
+      return res.status(200).json({
+        success: false,
         message: 'User ID already exists',
       })
     }
     await service.signUpUser(info)
-    res.status(201).json({message: 'User registered'})
+    res.status(201).json({success: true, message: 'User registered'})
   } catch (error) {
     console.error(error)
     res.status(500).json({
@@ -25,11 +26,13 @@ const getUserCheckAccount = async (req, res) => {
   try {
     const user = await service.getUserByAccount(account)
     if (user) {
-      return res.status(409).json({
+      return res.status(200).json({
+        success: false,
         message: 'User ID already exists',
       })
     }
     res.status(200).json({
+      success: true,
       message: 'User ID does not exist',
     })
   } catch (error) {
@@ -44,14 +47,14 @@ const postUserLogIn = async (req, res) => {
   const {account, password} = req.body
   try {
     const user = await service.getUserByAccount(account)
-    if (!user) return res.status(401).json({message: 'Invalid ID or password'})
+    if (!user) return res.status(200).json({success: false, message: 'Invalid ID or password'})
 
     const successLogIn = await user.checkPassword(password)
-    if (!successLogIn) return res.status(401).json({message: 'Invalid ID or password'})
+    if (!successLogIn) return res.status(200).json({success: false, message: 'Invalid ID or password'})
 
     const {accessToken, refreshToken} = await service.createToken(user._id)
     res.cookie('refreshToken', refreshToken, {httpOnly: true, secure: true, maxAge: 7 * 24 * 60 * 60 * 1000})
-    res.status(200).json({accessToken, uid: user._id})
+    res.status(200).json({success: true, accessToken, uid: user._id})
   } catch (error) {
     console.error(error)
     res.status(500).json({
@@ -66,20 +69,20 @@ const getUser = async (req, res) => {
   try {
     const {accessTokenValid, accessTokenError} = verifyAccessToken(accessToken, uid)
     if (accessTokenError) {
-      return res.status(400).json({message: accessTokenError})
+      return res.status(200).json({success: false, message: accessTokenError})
     }
     if (!accessTokenValid) {
       const {refreshTokenValid, refreshTokenError} = service.verifyRefreshToken({uid, refreshToken})
       if (!refreshTokenValid) {
-        return res.status(400).json({message: refreshTokenError})
+        return res.status(200).json({success: false, message: refreshTokenError})
       }
     }
     const {newAccessToken, newRefreshToken} = service.createToken(uid)
     const user = await service.getUserByUid(uid)
-    if (!user) return res.status(401).json({message: 'User not found'})
+    if (!user) return res.status(200).json({success: false, message: 'User not found'})
 
     res.cookie('refreshToken', newRefreshToken, {httpOnly: true, secure: true})
-    res.status(200).json({accessToken: newAccessToken, user: {name: user.name}})
+    res.status(200).json({success: true, accessToken: newAccessToken, user: {name: user.name}})
   } catch (error) {
     console.error(error)
     res.status(500).json({message: 'Fail to Find User'})
