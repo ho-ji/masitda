e911b4f72d7d433d4581dda22777c61d44cf1d37
ho-ji <wkdd0205@gmail.com>
2024-06-06 01:04:25 +0900

    feat: cookie보내기 위한 withCredentials옵션/인증헤더/사용자정보API/토큰검증API 추가

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -2,10 +2,16 @@ import instance from 'api'
 
 export const postLoginAPI = async (account, password) => {
   try {
-    const result = await instance.post('/user/login', {
-      account,
-      password,
-    })
+    const result = await instance.post(
+      '/user/login',
+      {
+        account,
+        password,
+      },
+      {
+        withCredentials: true,
+      }
+    )
     return result
   } catch (error) {
     throw error
@@ -29,6 +35,37 @@ export const postSignUpAPI = async (info) => {
     })
     return result
   } catch (error) {
+    throw error
+  }
+}
+
+export const getUserInformationAPI = async (accessToken) => {
+  const uid = localStorage.getItem('uid')
+  try {
+    const result = await instance.get(`/user/${uid}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      withCredentials: true,
+    })
+    return result
+  } catch (error) {
+    throw error
+  }
+}
+
+export const getVerifyTokenAPI = async (accessToken) => {
+  const uid = localStorage.getItem('uid')
+  try {
+    const result = await instance.get(`/user/login/${uid}`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      withCredentials: true,
+    })
+    return result
+  } catch (error) {
+    console.log(error)
     throw error
   }
 }
