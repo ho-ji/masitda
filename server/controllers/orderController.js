f7ea2372383ecab8161ef2db3abd4db9028614c8
ho-ji <wkdd0205@gmail.com>
2024-06-14 00:02:48 +0900

    feat: 최근 주문내역 API 추가

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -45,7 +45,30 @@ const getOrder = async (req, res) => {
   }
 }
 
+const getRecentOrder = async (req, res) => {
+  const uid = req.params.uid
+  const isLogIn = !uid.startsWith('guest')
+  const accessToken = req.headers.authorization?.split('Bearer ')[1]
+  const refreshToken = req.cookies?.refreshToken
+  const page = parseInt(req.query.page)
+  try {
+    if (!isLogIn) return res.status(200).json({success: false, message: 'No Login'})
+    const result = await userService.verifyToken({uid, accessToken, refreshToken})
+    if (!result.success) {
+      return res.status(200).json(result)
+    }
+    const {accessToken: newAccessToken, refreshToken: newRefreshToken} = await userService.createToken(uid)
+    const orderList = await service.getRecentOrderList(uid, page)
+    res.cookie('refreshToken', newRefreshToken, {httpOnly: true, secure: true})
+    return res.status(200).json({success: true, accessToken: newAccessToken, orderList})
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({message: 'Fail to load order list'})
+  }
+}
+
 module.exports = {
   postOrder,
   getOrder,
+  getRecentOrder,
 }
