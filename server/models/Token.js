4577e0e4027556a985d03ba2807b3e3f6b39b00a
ho-ji <wkdd0205@gmail.com>
2024-06-21 19:23:11 +0900

    refactor: token스키마 expires index설정

--- server/models/Token.js
+++ server/models/Token.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const tokenSchema = new mongoose.Schema({
   uid: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User'},
   refreshToken: {type: String, required: true},
-  expiresAt: {type: Date, required: true},
+  expiresAt: {type: Date, required: true, index: {expires: '0s'}},
 })
 
 const Token = mongoose.model('Token', tokenSchema)
